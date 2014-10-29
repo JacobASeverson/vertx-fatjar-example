6685bdfd67edde3c1dc28f64cc4fa985bfcb9cdf
jacobaseverson <jacoba.severson@gmail.com>
2014-10-29 18:49:46 -0500

    updating the hello verticle and adding the world jar to the repo

--- hello-mod/src/main/java/com/objectpartners/hello/HelloVerticle.java
+++ hello-mod/src/main/java/com/objectpartners/hello/HelloVerticle.java
@@ -1,49 +1,33 @@
-/*
- * Copyright 2013 Red Hat, Inc.
- *
- * Red Hat licenses this file to you under the Apache License, version 2.0
- * (the "License"); you may not use this file except in compliance with the
- * License.  You may obtain a copy of the License at:
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
- * License for the specific language governing permissions and limitations
- * under the License.
- *
- *
- */
 package com.objectpartners.hello;
 
 
+import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
-/*
- * This is a simple Java verticle which receives `ping` messages on the event bus and sends back `pong` replies
- *
- * @author <a href="http://tfox.org">Tim Fox</a>
- */
 public class HelloVerticle extends Verticle {
 
-  public void start() {
+    public void start() {
 
-    final Logger logger = container.logger();
+        container.deployModule("com.objectpartners~world-mod~1.0.0-SNAPSHOT", new Handler<AsyncResult<String>>() {
+            @Override
+            public void handle(AsyncResult<String> event) {
+                if (event.succeeded()) {
+                    container.logger().info("successfully deployed the world module...");
+                } else {
+                    container.logger().info("world module did not deploy...");
+                    event.cause().printStackTrace();
+                }
+            }
+        });
 
-    vertx.eventBus().registerHandler("ping-address", new Handler<Message<String>>() {
-      @Override
-      public void handle(Message<String> message) {
-        message.reply("pong!");
-        logger.info("Sent back pong");
-      }
-    });
-
-
-    logger.info("PingVerticle started");
-
-  }
+        vertx.eventBus().send("world-address", "Hello", new Handler<Message<String>>() {
+            @Override
+            public void handle(Message<String> event) {
+                container.logger().info("Received message: " + event.body());
+            }
+        });
+    }
 }
