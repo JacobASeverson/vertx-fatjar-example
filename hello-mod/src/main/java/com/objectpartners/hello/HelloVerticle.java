2fa904b8091795109384d22e8237e66e86841e63
jacobaseverson <jacoba.severson@gmail.com>
2014-11-03 18:33:11 -0600

    more cleanup

--- hello-mod/src/main/java/com/objectpartners/hello/HelloVerticle.java
+++ hello-mod/src/main/java/com/objectpartners/hello/HelloVerticle.java
@@ -4,7 +4,6 @@ package com.objectpartners.hello;
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
-import org.vertx.java.core.logging.Logger;
 import org.vertx.java.platform.Verticle;
 
 public class HelloVerticle extends Verticle {
